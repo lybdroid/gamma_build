c13ebc3e46ea189fc3c4b4cceea400e4d68b7581
lybxlpsv <lybxlpsv@users.noreply.github.com>
2021-04-12 21:11:09 +0700

    change gcc branch

--- prepare_env.sh
+++ prepare_env.sh
@@ -2,4 +2,4 @@
 
 sudo apt install bc
 git clone --depth=1 https://github.com/lybxlpsv/android_kernel_lge_msm8996.git -b gamma-pie kernel
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b lld-integration gcc64
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-master gcc64
